565939089c6d0c292c0957c3c11fe91bcf8bdf3a
Vishal Juyal <vishaljuyal237@gmail.com>
2024-12-01 09:52:30 -0800

    added lazy loading

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,17 @@
-import Weather from "@/components/weather";
+import Weather from "@/components/weather/WeatherPage";
+import Head from "next/head";
 
 export default function Home() {
-  return <Weather />;
+  return (
+    <>
+      <Head>
+        <title>Weather App | Check Today's Weather</title>
+        <meta
+          name="description"
+          content="Get the latest weather updates, including temperature, condition, and more. Stay informed with accurate weather forecasts."
+        />
+      </Head>
+      <Weather />
+    </>
+  );
 }
--- src/components/Listing/WeatherListing.jsx
+++ src/components/Listing/WeatherListing.jsx
@@ -1,51 +1,12 @@
-"use client";
-import { useState, useCallback } from "react";
-import axios from "axios";
-import "../../css/weather.css";
-import { fetchData, postData } from "../common/apiHelper";
+import React from "react";
+import { postData } from "../../common/apiHelper";
 
-export default function Weather() {
-  const [weatherData, setWeatherData] = useState([]);
-  const [errorProviders, setErrorProviders] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
+export default function WeatherListing({
+  weatherData,
+  setWeatherData,
+  errorProviders,
+}) {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-
-  const fetchWeather = useCallback(async () => {
-    try {
-      const lat = 19.076;
-      const lon = 72.8777;
-      setErrorMessage("");
-      const { data } = await fetchData(`${apiUrl}/api/weather`, {
-        lat,
-        lon,
-      });
-      if (data?.status === "success") {
-        const weather = data?.data?.weatherData.filter((w) => !w.error);
-        const errors = data?.data?.weatherData.filter((w) => w.error);
-
-        setErrorProviders(
-          errors.map((e) => ({
-            provider: e.provider,
-            errorMessage: e.errorMessage || "Unknown error",
-          }))
-        );
-
-        const mergedData = weather.map((w) => ({
-          ...w,
-          pinned: data?.data?.pinnedProviders.some(
-            (p) => p.provider === w.provider
-          ),
-        }));
-
-        setWeatherData(mergedData);
-      }
-    } catch (err) {
-      console.error("Error fetching weather data:", err?.message);
-      setErrorMessage(
-        "Something went wrong while fetching weather data. Please try again later."
-      );
-    }
-  }, [apiUrl]);
 
   const togglePin = async (weather, isPinned) => {
     try {
@@ -68,14 +29,7 @@ export default function Weather() {
   };
 
   return (
-    <div className="container">
-      <header className="header">
-        <h1>Check Today's Weather</h1>
-        <button className="button" onClick={fetchWeather}>
-          Fetch Weather Data
-        </button>
-      </header>
-      {errorMessage && <div className="error-message">{errorMessage}</div>}
+    <div>
       <table className="weatherTable">
         <thead>
           <tr>
